da432cd4fdcf645a07816528b0302caa1f82b5fb
GehanS <gehankavi@gmail.com>
2017-10-15 19:43:39 +1100

    The test files for debugging

---
+++ test files/DiceValueTest.java
@@ -0,0 +1,65 @@
+package tests;
+
+import com.gehan.DiceValue;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceValueTest {
+
+    @Test
+    public void TestToString(){
+
+        DiceValue diceValue = DiceValue.SPADE;
+
+        String sDiceValue = diceValue.toString(diceValue);
+        assertEquals(true, sDiceValue.equals("Spade"));
+
+        /*
+            Anchor Test
+         */
+        diceValue = DiceValue.ANCHOR;
+        sDiceValue = diceValue.toString(diceValue);
+        assertEquals(true, sDiceValue.equals("Anchor"));
+
+        /*
+            CLUB Test
+         */
+        diceValue = DiceValue.CLUB;
+        sDiceValue = diceValue.toString(diceValue);
+        assertEquals(true, sDiceValue.equals("Club"));
+
+        /*
+            CROWN test
+         */
+        diceValue = DiceValue.CROWN;
+        sDiceValue = diceValue.toString(diceValue);
+        assertEquals(true, sDiceValue.equals("Crown"));
+
+
+        /*
+            Hearts Test
+         */
+        diceValue = DiceValue.HEART;
+        sDiceValue = diceValue.toString(diceValue);
+        assertEquals(true, sDiceValue.equals("Heart"));
+
+        /*
+            Diamonds Test
+         */
+        diceValue = DiceValue.DIAMOND;
+        sDiceValue = diceValue.toString(diceValue);
+        assertEquals(true, sDiceValue.equals("Diamond"));
+    }
+
+    @Test
+    public void TestGetRandom() throws Exception {
+        DiceValue diceValue = DiceValue.getRandom();
+        for (int i = 0; i < 50; i++) {
+            assertEquals(true, diceValue.ordinal() > -1);
+            assertEquals(true, diceValue.ordinal() < 6);
+            diceValue = DiceValue.getRandom();
+        }
+    }
+
+}
---
+++ test files/PlayerTest.java
@@ -0,0 +1,99 @@
+package tests;
+
+import com.gehan.Player;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    Player player;
+
+    public PlayerTest() {
+        player = new Player("Mike", 20);
+        assertEquals(0, player.getLimit());
+        assertEquals(20, player.getBalance());
+    }
+
+
+    @Test
+    public void TestReceiveWinnings() {
+        int balance = player.getBalance();
+        int winnings = 10;
+        player.receiveWinnings(winnings);
+        assertEquals(balance + winnings, player.getBalance());
+
+        player.setBalance(balance);
+    }
+
+	/* indirectly we can test the "balance exceeds limits" functions.
+     * We don't know what they're supposed to do or what their
+	 * purpose is exactly but one occurs in takeBet and has a
+	 * exception so we can do what the exception says and see if it
+	 * gets raised.
+	 */
+
+    @Test
+    public void TestTakeBetNegative() {
+        int bet = -1;
+        int balance = player.getBalance();
+        try {
+            player.takeBet(bet);
+            assertEquals(true, false); //if we get to this point, the exception was not thrown.
+        } catch (IllegalArgumentException e) {
+            assertEquals(true, e.getMessage().equals("Bet cannot be negative."));
+        } finally {
+            player.setBalance(balance);
+			/*just in case it changed - in finally block so it gets put back
+			 * whether or not an exception was thrown (so it won't affect the
+			 * other tests)
+			 */
+        }
+    }
+
+
+    @Test
+    public void TestTakeBetGood() {
+        int balance = player.getBalance();
+        assertEquals(true, balance > 2); //so we can make sure we get a positive bet that is smaller than balance
+        int bet = balance - 1;
+
+        boolean bThrewException = true;
+        try {
+            player.takeBet(bet);
+            bThrewException = false; //only gets here if no exception thrown.
+        } catch (IllegalArgumentException e) {
+            assertEquals(true, false);//don't want an exception thrown
+        } finally {
+            assertEquals(false, bThrewException);
+            player.setBalance(balance);
+			/*just in case it changed - in finally block so it gets put back
+			 * whether or not an exception was thrown (so it won't affect the
+			 * other tests)
+			 */
+        }
+    }
+
+    @Test
+    public void TestTakeBetBig() {
+        int limit = player.getLimit();
+        int balance = player.getBalance();
+        player.setLimit(5);
+        int bet = balance - player.getLimit() + 1;
+
+        try {
+            player.takeBet(bet);
+            assertEquals(true, false); //if we get to this point, the exception was not thrown.
+        } catch (IllegalArgumentException e) {
+            assertEquals(true, e.getMessage().equals("Placing bet would go below limit."));
+        } finally {
+            player.setBalance(balance);
+            player.setLimit(limit);
+			/* just in case it changed - in finally block so it gets put back
+			 * whether or not an exception was thrown (so it won't affect the
+			 * other tests)
+			 */
+        }
+    }
+
+}
